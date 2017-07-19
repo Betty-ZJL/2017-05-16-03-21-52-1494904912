bd50432576671a4d4c0d3342336d40bc9d07b617
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-19 11:35:27 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
 module.exports = function main(email, suffixes) {
-  // Write your code here
+  var suf=email.substring(semail.indexOf('@')+1)
+  if (suffixes.indexOf(suf)>-1)
+      return true;
+  else 
+      return false;
 };
